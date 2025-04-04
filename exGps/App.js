66c1bfb00fd8889993e7fade71213bc7cf971ac8
Taiyo <103234811+taiyotk@users.noreply.github.com>
2025-04-04 23:22:54 +0000

    Aula gps com navegação

--- exGps/App.js
+++ exGps/App.js
@@ -1,52 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import * as Location from 'expo-location';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './screens/HomeScreen';
+import LocationScreen from './screens/LocationScreen'; // Corrigido para LocationScreen
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  // Estado para armazenar os dados de localização
-  const [location, setLocation] = useState(null);
-  // Função para solicitar permissão e obter a localização atual
-  const getLocation = async () => {
-    // Solicita permissão para acessar a localização em primeiro plano
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('Permissão de localização negada');
-      return;
-    }
-    // Se a permissão for concedida, obtém a posição atual do dispositivo
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-  };
-  // Função para atualizar a localização ao pressionar o botão
-  const handleUpdateLocation = () => {
-    getLocation(); // Chama a função para atualizar a localização
-  };
   return (
-    <View style={styles.container}>
-      {location ? (
-        // Caso a localização já tenha sido obtida, exibe os dados e um botão para atualizar
-        <View>
-          <Text style={styles.text}>
-            Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
-          </Text>
-          <Button title="Atualizar Localização" onPress={handleUpdateLocation} />
-        </View>
-      ) : (
-        // Se a localização ainda não foi obtida, exibe um botão para solicitar a localização
-        <Button title="Obter Localização" onPress={handleUpdateLocation} />
-      )}
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Localização" component={LocationScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center', // Centraliza verticalmente o conteúdo
-    alignItems: 'center', // Centraliza horizontalmente o conteúdo
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-});
+
